0fed1f9d12d9445d8736dfea90707664379cd729
Zahra Mehedi <zahra.mehedi@gmail.com>
2023-09-10 14:25:32 -0500

    Configured for deployment

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,7 +5,7 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/zahramehedi/profiles-rest-api.git'
 
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/Users/zahramehedi/Downloads/Workspace/profile-rest-api'
 
 # Set Ubuntu Language
 locale-gen en_GB.UTF-8
--- deploy/update.sh
+++ deploy/update.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/Users/zahramehedi/Downloads/Workspace/profile-rest-api'
 
 cd $PROJECT_BASE_PATH
 git pull
